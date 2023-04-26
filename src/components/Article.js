b9dcf0a56f0887d1d0ae54fdceb30099752f0966
Bwaweru12345 <bryanwaweru1076@example.com>
2023-04-26 10:49:54 +0300

    Complete assignment

---
+++ src/components/Article.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import blogData from "../data/blog";
+// import ArticleItem from "../components/ArticleList"
+
+
+function Article({title, date="January 1, 1970", preview, minutes}) {
+   
+  return (
+    <div className="Header">
+        <article className="article">
+            <h3>{title}</h3>
+            <small>{date}{minutes} minutes read</small>
+            <p>{preview}</p>
+        </article>
+     
+    </div>
+  );
+}
+
+export default Article;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,42 @@
+import React from "react";
+// import blogData from "../data/blog";
+import Article from "./Article";
+
+// console.log(blogData);
+
+function ArticleList({ posts }) {
+    // console.log(posts);
+    let postItem = posts.map((post) => (
+      <Article 
+      key = {post.id}
+      title = {post.title}
+      date = {post.date}
+      preview = {post.preview}/>
+    ))
+    
+    
+    return (
+      <main>
+        {postItem}
+      </main>
+      // <div id="posts">
+      //   <h2> Posts</h2>
+      //   <div id="post-list">
+      //    {postItem}
+      //   </div>
+      // </div>
+    );
+  }
+
+// function ArticleList({name}) {
+//   return (
+//     <div className="Header">
+//         <main className="header">
+//             <h1>{name}</h1>
+//         </main>
+     
+//     </div>
+//   );
+// }
+
+export default ArticleList;
